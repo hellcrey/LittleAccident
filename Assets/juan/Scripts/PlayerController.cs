656e72e12e1b10c87af4c81a787b5468567cc00d
hellcrey <109318827+hellcrey@users.noreply.github.com>
2024-03-20 16:43:27 -0500

    Scale when take powerUp

--- Assets/juan/Scripts/PlayerController.cs
+++ Assets/juan/Scripts/PlayerController.cs
@@ -4,13 +4,16 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-
+    float scale = 2;
     private void OnTriggerEnter(Collider other)
     {
-      
-            if (other.gameObject.CompareTag("PowerUp"))
-            {
-                Destroy(other.gameObject);
+
+        if (other.gameObject.CompareTag("PowerUp"))
+        {
+            Destroy(other.gameObject);
+            transform.localScale= new Vector3(scale, scale, scale);
+            scale++;
+            Debug.Log(scale);
 
         }
         else if (other.gameObject.CompareTag("Enemy"))
