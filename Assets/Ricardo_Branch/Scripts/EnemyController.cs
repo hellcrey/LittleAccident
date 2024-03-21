b812ff9122f34dc42e6dd7d0f3d3b4d4975c31be
Ibowa <162346100+Ibowa@users.noreply.github.com>
2024-03-21 10:57:01 -0500

    Enemy controller AI Navmesh
    Enemigo funcional con navmesh y maquina de estados

---
+++ Assets/Ricardo_Branch/Scripts/EnemyController.cs
@@ -0,0 +1,108 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyController : MonoBehaviour
+{
+    public int routine;
+    public float timer;
+    public Animator animator;
+    public Quaternion angle;
+    public float degree;
+
+    public GameObject target;
+    public bool isAttacking;
+    public EnemyRange range;
+
+    public NavMeshAgent agent;
+    public float attackDistance;
+    public float sightDistance;
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        target = GameObject.Find("FirstPersonPlayer");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        EnemyBehaivor();
+    }
+    public void EnemyBehaivor()
+    {
+        if (Vector3.Distance(transform.position, target.transform.position) > sightDistance)
+        {
+            agent.enabled = false;
+            animator.SetBool("run", false);
+            timer += 1 * Time.deltaTime;
+            if (timer >= 4)
+            {
+                routine = Random.Range(0, 2);
+                timer = 0;
+            }
+            switch (routine)
+            {
+                case 0:
+                    animator.SetBool("walk", false);
+                    break;
+
+                case 1:
+                    degree = Random.Range(0, 360);
+                    angle = Quaternion.Euler(0, degree, 0);
+                    routine++;
+                    break;
+
+                case 2:
+                    transform.rotation = Quaternion.RotateTowards(transform.rotation, angle, 0.5f);
+                    transform.Translate(Vector3.forward * speed * Time.deltaTime);
+                    animator.SetBool("walk", true);
+                    break;
+            }
+        }
+        else
+        {
+            var lookPos = target.transform.position - transform.position;
+            lookPos.y = 0;
+            var rotation = Quaternion.LookRotation(lookPos);
+
+            agent.enabled = true;
+            agent.SetDestination(target.transform.position);
+
+            if(Vector3.Distance(transform.position, target.transform.position) > attackDistance && !isAttacking)
+            {
+                animator.SetBool("walk", false);
+                animator.SetBool("run", true);
+            }
+            else
+            {
+                if (!isAttacking)
+                {
+                    transform.rotation = Quaternion.RotateTowards(transform.rotation, rotation, 1);
+                    animator.SetBool("walk", false);
+                    animator.SetBool("run", true);
+                }
+            }
+        }
+        if(isAttacking)
+        {
+            agent.enabled = false;  
+        }
+
+    }
+    public void EndAttack()
+    {
+        if (Vector3.Distance(transform.position, target.transform.position) > attackDistance + 0.2f)
+        {
+            animator.SetBool("attack", false);
+            
+        }
+        isAttacking = false;
+        range.GetComponent<CapsuleCollider>().enabled = true;
+
+    }
+
+}
